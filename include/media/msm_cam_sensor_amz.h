e5392705166141726444869fcbd8e74a5f46fd85
Hashcode <hashcode0f@gmail.com>
2016-12-13 13:47:08 -0800

    msm_camera_amz: allow larger sequence reg setting data
    With recent CVEs applied, firmware reg settings were being rejected
    due to size restraints.  Adjust the max reg setting size back to
    something acceptible for HDX firmware.
    
    NOTE: This is a hack as firmware could request reg settings that are
    larger than 50k.  The actual size may need to be adjusted for Apollo
    as this was only tested on Thor HW.
    
    Change-Id: I7bea61933f07fb90b55eecf058f42b398e179055

--- include/media/msm_cam_sensor_amz.h
+++ include/media/msm_cam_sensor_amz.h
@@ -9,7 +9,9 @@
 #include <linux/i2c.h>
 
 #define I2C_SEQ_REG_SETTING_MAX   5
-#define I2C_SEQ_REG_DATA_MAX      256
+/* Fix buffer size for firmware loaded reg settings */
+#define I2C_SEQ_REG_DATA_MAX      (50*1024)
+#endif
 #define I2C_REG_DATA_MAX          (8*1024)
 #define MAX_CID                   16
 
